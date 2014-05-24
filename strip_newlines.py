a85a241e9d7375873c752ba2490aa8319fa4a967
Nathan Matias <natematias@gmail.com>
2014-05-24 08:55:39 -0400

    fixed newline stripping code

--- strip_newlines.py
+++ strip_newlines.py
@@ -4,7 +4,7 @@ f = open(sys.argv[1], "r")
 text = f.read()
 #text = text.replace(".",".\n")
 f.close()
-text = text.replace("\r\n","").replace("\t"," ").replace("  "," ").replace("  "," ").replace("  ", " ").replace("  "," ").replace("  "," ")
+text = text.replace("\r\n"," ").replace("\t"," ").replace("  "," ").replace("  "," ").replace("  ", " ").replace("  "," ").replace("  "," ") + "\n"
 
 f = open(sys.argv[2],"w")
 f.write(text)
